fb5b15f83e73dc9d69a503e525ae42b0d9f17e90
DanielRl9 <dsp4346895@outlook.com>
2025-12-09 14:23:24 -0600

    creacion de la barra de navegacion

---
+++ app/src/main/java/com/example/blackmouth/ui/Screens/Components/NavBar.kt
@@ -0,0 +1,84 @@
+package com.example.blackmouth.ui.Screens.Components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.blackmouth.ui.theme.BlackMouthTheme
+
+
+@Composable
+fun NavBar(){
+    NavigationBar(
+        modifier = Modifier
+            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+    ) {
+        NavigationBarItem(
+            selected = false,
+
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(25.dp)
+                )
+            },
+            onClick = {
+
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(35.dp)
+                )
+            },
+            onClick = {
+
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(25.dp)
+                )
+            },
+            onClick = {
+
+            }
+        )
+
+    }
+}
+
+
+@Preview
+@Composable
+fun NavBarView(){
+    BlackMouthTheme {
+        NavBar()
+    }
+}
