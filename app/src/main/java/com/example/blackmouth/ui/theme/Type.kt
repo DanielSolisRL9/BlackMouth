4448280e7cf01a2073949eb297abe9ef3b2250c7
MiguelMH04 <mmh79381@lasallebajio.edu.mx>
2025-12-09 22:42:18 -0600

    Implementación de las fuentes lemonmilk en Type.kt

--- app/src/main/java/com/example/blackmouth/ui/theme/Type.kt
+++ app/src/main/java/com/example/blackmouth/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.example.blackmouth.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-
-// Set of Material typography styles to start with
+import com.example.blackmouth.R
+val lemonMilkFontFamily = FontFamily(
+    Font(R.font.lemonmilklight, FontWeight.Light),
+    Font(R.font.lemonmilkregular, FontWeight.Normal),
+    Font(R.font.lemonmilkbold, FontWeight.Bold)
+)
 val Typography = Typography(
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = lemonMilkFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+
+    titleLarge = TextStyle(
+        fontFamily = lemonMilkFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp
+    ),
+
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = lemonMilkFontFamily,
+        fontWeight = FontWeight.Light,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
